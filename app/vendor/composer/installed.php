3e12e200f212673c9181ebc52b25a05cb41005ed
RudyVieira <rudy.vieira3@gmail.com>
2025-02-18 14:53:41 +0100

    commit de app/vendor

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/html',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce6900127c196f6fa6aaa7ceb668fe1e5563267f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/html' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce6900127c196f6fa6aaa7ceb668fe1e5563267f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
