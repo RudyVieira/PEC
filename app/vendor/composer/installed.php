d6e36e025b3e7aa40b101bb561ff712413d152b5
Senka <xsenka7@gmail.com>
2025-02-18 16:16:08 +0100

    add and change database

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/html',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '05309e72c78a55e9c3b9741c4b59d7c50683160a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/html' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '05309e72c78a55e9c3b9741c4b59d7c50683160a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
